d7bad3da33412beaf081bbc673ec6e27e963fd46
Tim Roediger <superdweebie@gmail.com>
2014-07-21 22:42:48 +1000

    Update gomi to use shard 5

--- src/Zoop/GomiModule/Module.php
+++ src/Zoop/GomiModule/Module.php
@@ -7,9 +7,6 @@
 
 namespace Zoop\GomiModule;
 
-use Zend\View\Model\JsonModel;
-use Zend\Mvc\MvcEvent;
-
 /**
  *
  * @license MIT
@@ -22,36 +19,5 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/../../../config/module.config.php';
-    }
-
-    public function onBootstrap(MvcEvent $event)
-    {
-        $events = $event->getTarget()->getEventManager();
-        $events->attach('dispatch.error', array($this, 'onDispatchError'), 100);
-    }
-
-    public function onDispatchError(MvcEvent $event)
-    {
-        $error = $event->getError();
-        if (!$error) {
-            return;
-        }
-
-        $request = $event->getRequest();
-        $headers = $request->getHeaders();
-        if (!$headers->has('Accept')) {
-            return;
-        }
-
-        $accept = $headers->get('Accept');
-        if (!$accept->match('application/json')) {
-            return;
-        }
-
-        $model = new JsonModel();
-
-        $event->setResult($model);
-        $event->stopPropagation();
-        return $model;
     }
 }
